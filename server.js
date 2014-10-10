94b120bb7143abac65ef47c9ea9afc1de15a3d56
ahmed maged <ahmaadmaged@gmail.com>
2014-10-10 15:42:15 +0200

    fixed a little mistake in placing ships

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ var handlers = {
         var player = room.getThisPlayer(user);
         var otherPlayer = room.getOtherPlayer(user);
         //set the grid
-        for(var i=0;i<data.grid;i++)
+        for(var i=0;i<data.grid.length;i++)
             data.grid[i] && player.grid.placeShipAt(i);
 
         //check set a ready flag
